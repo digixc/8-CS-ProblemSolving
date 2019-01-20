21c14bbffafbb92774bb451a6875239a259b6765
digixc <xiaohui.ellis@bourne-grammar.lincs.sch.uk>
2019-01-20 20:43:31 +0000

    added overview, eval etc

--- pages/js/navbar.js
+++ pages/js/navbar.js
@@ -9,3 +9,26 @@ $(function(){
                 }
         });
 });
+
+
+function overlayon(bgcol) {
+    var bgcol = bgcol;
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay").style.background = bgcol;
+    underlayoff();
+}
+
+function overlayoff() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function underlayon(bgcol) {
+    document.getElementById("underlay").style.display = "block";
+    document.getElementById("underlay").style.background = bgcol;
+    overlayoff();
+}
+
+function underlayoff() {
+ 
+        document.getElementById("underlay").style.display = "none";
+}
